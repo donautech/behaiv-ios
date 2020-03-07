f73707f18b787c0bf6e29d1e8970d1e5da9aca24
dmi3coder <dmi3coder@gmail.com>
2020-03-07 08:43:50 +0100

    Removed Example/Pods from folder

--- Example/Pods/Target Support Files/Behaiv/Behaiv-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Behaiv : NSObject
-@end
-@implementation PodsDummy_Behaiv
-@end
--- Example/Pods/Target Support Files/Pods-Behaiv_Example/Pods-Behaiv_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Behaiv_Example : NSObject
-@end
-@implementation PodsDummy_Pods_Behaiv_Example
-@end
